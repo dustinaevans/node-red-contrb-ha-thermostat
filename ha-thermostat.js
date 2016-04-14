53bdd953116f80ee6da45ed102bb3c9a430ca8a6
Dustin Evans <dustinaevans@gmail.com>
2016-04-14 15:01:33 -0500

    updates to thermostat

--- ha-thermostat.js
+++ ha-thermostat.js
@@ -13,3 +13,15 @@ module.exports = function (RED) {
 };
 
 //https://cache.freescale.com/files/microcontrollers/doc/ref_manual/DRM106.pdf
+
+//Accuracy of 0.1 °C
+//• Day-of-the-week, hours, and minutes calendar for the low-end version, and date, hours, and
+//minutes for the high-end version
+//• Temperature display in °C or °F
+//• Programmable set points for heating and cooling
+//• Programmable run and hold mode
+//• Fan on and auto mode
+//• Standard HVAC connections
+//• Current sensing switch and screw-terminals to measure power consumption
+//• HVAC LED indicators
+//• Contrast control for the high-end version
